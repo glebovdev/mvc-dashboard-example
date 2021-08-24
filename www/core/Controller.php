89245060620c44f8ca0b4d08f401f3cfa621bb23
glebovdev <hi@ilyaglebov.dev>
2021-08-24 20:33:20 +0300

    add Controller core class

---
+++ www/core/Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Core;
+
+/**
+ * Parent controller class. Can be used for common for every controller logic
+ */
+class Controller
+{
+    // Common logic to be used in every controller
+}
